d0754c5d5c41155f692a472e7e4d3ab141a8d57b
Beatriz Oliveira <ionebeatriz_po@hotmail.com>
2018-02-20 11:05:14 -0300

    Consulta de URL

---
+++ consulta.php
@@ -0,0 +1,25 @@
+<?php
+
+	function curl_info($url){
+		$ch = curl_init();
+		curl_setopt( $ch, CURLOPT_URL, $url );
+		curl_setopt( $ch, CURLOPT_HEADER, 1);
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
+        
+		$content = curl_exec( $ch );
+		$info = curl_getinfo( $ch );
+
+		return $info;
+	}
+
+	$site = 'https://codigital.oi.net.br';
+	$info = curl_info( $site );
+	if( $info['http_code']==200 ) {
+		echo '<u>'.$site . '</u> - <strong>está funcionando</strong><br />';
+	} else {
+		echo '<u>'.$site . '</u> - não está funcionando<br />';
+	}
+
+
